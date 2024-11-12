f314b76f18c14cba4b87055a8f3bd6f89620ad41
Murilo358 <murilobarbosa358@gmail.com>
2024-11-12 20:01:07 -0300

    Configured timeOut to DockerClientConfig

--- src/main/java/com/murilo358/docker_manager/config/DockerClientConfig.java
+++ src/main/java/com/murilo358/docker_manager/config/DockerClientConfig.java
@@ -3,11 +3,13 @@ package com.murilo358.docker_manager.config;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.RemoteApiVersion;
 import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
 @Configuration
 public class DockerClientConfig {
 
@@ -17,20 +19,30 @@ public class DockerClientConfig {
     @Bean
     public DockerClient buildDockerClient() {
 
-        DefaultDockerClientConfig.Builder dockerClientConfig = DefaultDockerClientConfig.createDefaultConfigBuilder();
+        DefaultDockerClientConfig.Builder dockerClientConfigBuilder = DefaultDockerClientConfig.createDefaultConfigBuilder();
 
         if (this.dockerSocketPath != null && this.dockerSocketPath.startsWith("unix://")) {
-            dockerClientConfig.withDockerHost(dockerSocketPath).withDockerTlsVerify(false);
+            dockerClientConfigBuilder.withDockerHost(dockerSocketPath)
+                    .withApiVersion(RemoteApiVersion.VERSION_1_24)
+                    .withDockerTlsVerify(false);
         }
 
-        DefaultDockerClientConfig build = dockerClientConfig.build();
-
+        DefaultDockerClientConfig dockerClientConfig = dockerClientConfigBuilder
+                .build();
+//
         ApacheDockerHttpClient dockerHttpClient = new ApacheDockerHttpClient.Builder()
-                .dockerHost(build.getDockerHost()).build();
-
-        return DockerClientBuilder.getInstance()
+                .dockerHost(dockerClientConfig.getDockerHost())
+                .maxConnections(5)
+                .connectionTimeout(Duration.ofMillis(300))
+                .responseTimeout(Duration.ofSeconds(3))
+                .build();
+//
+        DockerClient client = DockerClientBuilder.getInstance(dockerClientConfig)
                 .withDockerHttpClient(dockerHttpClient)
                 .build();
 
+        client.pingCmd().exec();
+
+        return client;
     }
 }
